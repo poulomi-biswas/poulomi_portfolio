f4232fe860f5d4d3df0652224ef24ae854ef336e
poulomi-biswas <94776150+poulomi-biswas@users.noreply.github.com>
2023-05-19 14:09:57 +0530

    Add files via upload

---
+++ Responsive-Portfolio-Website/js/app.js
@@ -0,0 +1,86 @@
+const header=document.querySelector("header");
+
+
+const first_skill=document.querySelector(".skill:first-child");
+const sk_counters=document.querySelectorAll(".counter span");
+const progress_bars=document.querySelectorAll(".skills svg circle");
+
+window.addEventListener("scroll",()=>{
+    if(!skillsPlayed)skillsCounter();
+});
+
+/* --------------- Grab elements from DOM --------------- */
+
+/* --------------- Sticky Navbar --------------- */
+function stickyNavbar(){
+    header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+window.addEventListener("scroll", stickyNavbar);
+
+/* --------------- Reveal Animation --------------- */
+let sr = ScrollReveal(
+    {
+    duration: 2500,
+    distance:"60px",
+    });
+    sr.reveal(".showcase-info", {delay: 200});
+    sr.reveal(".showcase-image", {origin: top, delay: 400});
+
+
+
+/* --------------- Skills Progress Bar Animation --------------- */
+function hasReached(el){
+    let topPosition = el.getBoundingClientRect().top;
+
+    if(window.innerHeight>=topPosition+ el.offsetHeight) return true;
+        return false;
+}
+function updateCount(num, maxNum){
+    let currentNum = +num.innerText;
+    if (currentNum < maxNum){
+        num.innerText = currentNum +1;
+        setTimeout(() => {
+            updateCount(num, maxNum);
+        }, 12);
+    }
+}
+let skillsPlayed = false;
+function skillsCounter(){
+    if(!hasReached(first_skill)) return;
+    skillsPlayed = true;
+    sk_counters.forEach((counter, i) => {
+        let target =+counter.dataset.target;
+        let strokeValue = 427 - 427 * (target / 100);
+
+        progress_bars[i].style.setProperty("--target", strokeValue);
+        setTimeout(() => {
+            updateCount(counter, target);
+        }, 400);
+    
+    
+});
+
+    sk_counters.forEach((counter, i) =>{
+        let target = +counter.dataset.target;
+        // let strokeValue =+counter.dataset.target;
+        let strokeValue =427 - 427 * (target / 100);
+        progress_bars[i].style.setProperty("--target", strokeValue);
+    });
+    progress_bars.forEach(
+        (p) => (p.style.animation="progress 2s ease-in-out forwards")
+        );
+}
+
+/* --------------- Services Counter Animation --------------- */
+
+/* --------------- Portfolio Filter Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Change Active Link On Scroll --------------- */
+
+/* --------------- Change Page Theme --------------- */
+
+/* --------------- Open & Close Navbar Menu --------------- */
